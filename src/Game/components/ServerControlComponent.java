a44dc67600ab890deffedc74f6d963526e7950d0
fastfists <fdenzell@gmail.com>
2018-10-29 08:06:30 -0500

    IDK what the fuck just happened but its all better now ... I hope

---
+++ src/Game/components/ServerControlComponent.java
@@ -0,0 +1,23 @@
+package Game.components;
+
+import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxgl.entity.components.PositionComponent;
+import javafx.geometry.Point2D;
+
+import java.util.HashMap;
+
+public class ServerControlComponent extends Component {
+
+    public static HashMap<Integer, Point2D> characterPositions;
+    private int id;
+    private PositionComponent position;
+
+    public ServerControlComponent(int id){
+        this.id = id;
+    }
+
+    public void onUpdate(){
+        Point2D pos = characterPositions.get(id);
+        position.setValue(pos);
+    }
+}
